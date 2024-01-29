37ae5b2ad4b9c5059203c668c8308f24ecd72178
IsmailMohamed125 <141660592+IsmailMohamed125@users.noreply.github.com>
2024-01-29 16:52:45 +0000

    Basic calculator functions

--- main.js
+++ main.js
@@ -26,3 +26,92 @@ function operate(firstNumber, operator, secondNumber) {
       return divide(firstNumber, secondNumber);
   }
 }
+
+const currentCalc = document.querySelector(".current-calc");
+const answer = document.querySelector(".answer");
+const digits = document.querySelectorAll(".digits");
+const operators = document.querySelectorAll(".operators");
+const equal = document.querySelector(".btn-eql");
+const clear = document.querySelector(".clear");
+const deleteBtn = document.querySelector(".delete");
+let calcArray = [];
+let isActionOne = true;
+let currentValue;
+let newAnswer;
+
+digits.forEach((digit) =>
+  digit.addEventListener("click", function () {
+    decCheck();
+    calcArray.push(`${digit.textContent}`);
+    changeDisplay();
+    console.log(calcArray);
+  })
+);
+
+function changeDisplay() {
+  if (calcArray.length === 0) {
+    answer.textContent = 0;
+  } else {
+    answer.textContent = calcArray.join("");
+  }
+}
+
+operators.forEach((op) => {
+  op.addEventListener("click", function () {
+    if (isActionOne) {
+      currentValue = +answer.textContent;
+      firstNumber = currentValue;
+      operator = op.textContent;
+      currentCalc.textContent = `${firstNumber} ${operator}`;
+      answer.textContent = 0;
+      calcArray = [];
+      // dec.disabled = false;
+      isActionOne = !isActionOne;
+    } else {
+      currentValue = +answer.textContent;
+      secondNumber = currentValue;
+      console.log(secondNumber, firstNumber, operator);
+      newAnswer = operate(firstNumber, operator, secondNumber);
+      console.log(newAnswer);
+      firstNumber = newAnswer;
+      operator = op.textContent;
+      currentCalc.textContent = `${firstNumber} ${operator}`;
+      answer.textContent = 0;
+      calcArray = [];
+      // dec.disabled = false;
+    }
+  });
+});
+
+equal.addEventListener("click", function () {
+  if (
+    firstNumber !== undefined &&
+    operator !== undefined &&
+    calcArray.length !== 0
+  ) {
+    secondNumber = +answer.textContent;
+    answer.textContent = operate(firstNumber, operator, secondNumber);
+    calcArray = [...answer.textContent];
+    // console.log(calcArray);
+    currentCalc.textContent = "";
+    reset();
+    // console.log(secondNumber, firstNumber);
+    isActionOne = true;
+  }
+});
+
+clear.addEventListener("click", function () {
+  reset();
+  calcArray = [];
+  // dec.disabled = false;
+  currentCalc.textContent = "";
+  answer.textContent = 0;
+  isActionOne = true;
+});
+
+deleteBtn.addEventListener("click", function () {
+  if (calcArray.length > 0) {
+    calcArray.pop();
+    changeDisplay();
+  }
+});
